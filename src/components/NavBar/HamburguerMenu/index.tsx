28e07603c9ddf60f7385db73853804ab52776721
cgbinho <cgbinho@gmail.com>
2021-10-11 08:21:24 -0300

    Removed Locale from Mobile version

--- src/components/NavBar/HamburguerMenu/index.tsx
+++ src/components/NavBar/HamburguerMenu/index.tsx
@@ -14,11 +14,6 @@ export function HamburguerMenu({ content }) {
 
   const close = () => setOpenBurguer(false);
 
-  const handleSignOut = async () => {
-    // await signOut();
-    router.push('/');
-  };
-
   const MenuItem = ({ label, path }) => {
     return (
       <Link href={path} passHref>
@@ -27,42 +22,16 @@ export function HamburguerMenu({ content }) {
     );
   };
 
-  const GuestMenu = () => {
-    return (
-      <>
-        <MenuItem {...{ label: `${content.sign_in}`, path: '/sign-in' }} />
-        <Link href="/sign-up" passHref>
-          <StyledLink onClick={close}>
-            <StyledButton primary width="100%">
-              {content.sign_up}
-            </StyledButton>
-          </StyledLink>
-        </Link>
-      </>
-    );
-  };
-
-  const LoggedMenu = () => {
-    return (
-      <>
-        <MenuItem {...{ label: `${content.orders}`, path: '/orders' }} />
-        <StyledLink onClick={handleSignOut}>{content.sign_out}</StyledLink>
-      </>
-    );
-  };
-
   return (
     <div ref={node}>
       <StyledMenu open={openBurguer}>
         <MenuItem {...{ label: `${content.home}`, path: '/#home' }} />
-        {/* {!user && <GuestMenu />} */}
         <MenuItem
           {...{ label: `${content.experience}`, path: '/#experience' }}
         />
         <MenuItem {...{ label: `${content.projects}`, path: '/#projects' }} />
         <MenuItem {...{ label: `${content.articles}`, path: '/articles' }} />
         <MenuItem {...{ label: `${content.about}`, path: '/#about' }} />
-        {/* {user && <LoggedMenu />} */}
       </StyledMenu>
       <HamburgerButton {...{ openBurguer, setOpenBurguer }} />
     </div>
