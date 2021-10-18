9ebfbc2e36cf04719e400b37836d481cde82391b
cgbinho <cgbinho@gmail.com>
2021-10-18 09:26:15 -0300

    Fixed Language Menu

--- src/components/Form/DropdownUserMenu/index.tsx
+++ src/components/Form/DropdownUserMenu/index.tsx
@@ -1,17 +1,43 @@
-import { Menu, MenuDivider, MenuItem } from '@szhsin/react-menu';
-import '@szhsin/react-menu/dist/index.css';
-import '@szhsin/react-menu/dist/theme-dark.css';
-import { useRouter } from 'next/router';
 import React from 'react';
+import {
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuHeader,
+  MenuItem,
+  SubMenu,
+} from '@szhsin/react-menu';
 import { HiChevronDown } from 'react-icons/hi';
 import { DropdownContainer } from './styles';
+
+import { useRouter } from 'next/router';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/theme-dark.css';
 
 export function DropdownUserMenu() {
   const router = useRouter();
 
+  const { locale } = router;
+
+  const contentLanguage = {
+    en: {
+      title: 'Language',
+      language01: 'Portuguese (pt-Br)',
+      language02: 'English (en-Us)',
+    },
+    'pt-BR': {
+      title: 'Linguagem',
+      language01: 'Português (pt-Br)',
+      language02: 'Inglês (en-Us)',
+    },
+  };
+
+  const content = contentLanguage[locale];
+
   return (
     <DropdownContainer>
       <Menu
+        className="rc-menu-container--theme-dark"
         menuButton={
           <a className="dropdown_button">
             <p>{router.locale}</p>
@@ -20,18 +46,19 @@ export function DropdownUserMenu() {
         }
         theming="dark"
       >
+        <MenuHeader>{content.title}</MenuHeader>
+        <MenuDivider />
         <MenuItem
           onClick={() => router.push('./', './', { locale: 'pt-BR' })}
           value="pt-BR"
         >
-          Portuguese (pt-BR)
+          {content.language01}
         </MenuItem>
-        <MenuDivider />
         <MenuItem
           onClick={() => router.push('./', './', { locale: 'en' })}
           value="en"
         >
-          English (en-Us)
+          {content.language02}
         </MenuItem>
       </Menu>
     </DropdownContainer>
--- src/components/Form/DropdownUserMenu/styles.ts
+++ src/components/Form/DropdownUserMenu/styles.ts
@@ -6,18 +6,19 @@ export const DropdownContainer = styled.div`
     flex-direction: row;
     align-items: center;
   }
+
   .rc-menu-container--theme-dark .rc-menu {
     /* color: #cad1d8; */
-    color: var(--primary);
+    /* color: var(--primary); */
     /* background-color: #22262c; */
     background-color: var(--gray-100);
-    border: 1px solid #333;
+    /* border: 1px solid #333; */
     box-shadow: 0 2px 9px 3px rgba(0, 0, 0, 0.25);
   }
   .rc-menu-container--theme-dark .rc-menu__arrow {
     /* background-color: #22262c; */
-    border-left-color: #333;
-    border-top-color: #333;
+    /* border-left-color: #333; */
+    /* border-top-color: #333; */
   }
   .rc-menu-container--theme-dark .rc-menu__item--hover {
     /* background-color: #31363c; */
@@ -25,15 +26,20 @@ export const DropdownContainer = styled.div`
     color: var(--scent-450);
   }
   .rc-menu-container--theme-dark .rc-menu__item--active {
-    background-color: #007bff;
+    background-color: var(--gray-100);
   }
   .rc-menu-container--theme-dark .rc-menu__item--focusable {
     background-color: inherit;
+    background-color: #fff;
   }
   .rc-menu-container--theme-dark .rc-menu__item--disabled {
     color: #666;
   }
   .rc-menu-container--theme-dark .rc-menu__divider {
     background-color: #3a3a3a;
+  }
+  .szh-menu--dir-bottom {
+    background-color: var(--gray-100);
+    top: 16px !important;
   }
 `;
